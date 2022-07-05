e7aa843d15351746e3d8562e7a59e82f0705c27c
SibuleleMboyi <smboyi2016@gmail.com>
2022-07-05 15:28:56 +0200

    get configuration from application properties

--- src/main/java/com/in28minutes/microservices/limitsservice/controllers/LimitsController.java
+++ src/main/java/com/in28minutes/microservices/limitsservice/controllers/LimitsController.java
@@ -1,16 +1,25 @@
 package com.in28minutes.microservices.limitsservice.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.in28minutes.microservices.limitsservice.configurations.Configuration;
 import com.in28minutes.microservices.limitsservice.models.Limits;
 
 @RestController
 public class LimitsController {
 
+    @Autowired
+    private Configuration configuration;
+    
     @GetMapping("/limits")
     public Limits retrieveLimits() {
         return new Limits(1, 100);
     }
 
+    @GetMapping("/limits-properties")
+    public Limits retrieveAppPropertiesLimits() {
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
+    }
 }
